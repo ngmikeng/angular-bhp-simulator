8dad0cd3e5c096840341fbed3847c448aea7f2c4
Minh Nguyen <minhn@j317.com>
2026-01-30 12:31:51 +0700

    feat: configure Angular Material theming with light/dark themes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,15 @@ import {
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideEcharts } from 'ngx-echarts';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideAnimations(),
+    provideEcharts(),
+  ],
 };
