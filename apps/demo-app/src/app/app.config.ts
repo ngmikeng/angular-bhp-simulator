65e1004209ffb631c26a6658ca386d021af42c26
MN Research <ngmikeng@gmail.com>
2026-01-31 09:27:54 +0700

    feat: update app configuration and styles for improved layout and icon integration

--- apps/demo-app/src/app/app.config.ts
+++ apps/demo-app/src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { appRoutes } from './app.routes';
+
 // import echarts core
 import * as echarts from 'echarts/core';
 import { provideEchartsCore } from 'ngx-echarts';
-import { appRoutes } from './app.routes';
+import { LineChart } from 'echarts/charts';
+import { GridComponent, TitleComponent, LegendComponent, TooltipComponent, DataZoomComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+echarts.use([LineChart, GridComponent, TitleComponent, LegendComponent, TooltipComponent, DataZoomComponent, CanvasRenderer]);
 
 export const appConfig: ApplicationConfig = {
   providers: [
